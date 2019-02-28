17ed29852ec7f3864aa8536d83bdc41c7511a2b7
xiaozhupig <xuexizw@126.com>
2019-02-28 22:33:32 +0800

    Update Method.java

--- Method.java
+++ Method.java
@@ -4,6 +4,7 @@ public class Method {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
             System.out.println("i:"+ i);
+             System.out.println("test");
         }
     }
 }
